92baf0fb34b4f010887e5d5b0fc9219b48c5581d
jojoford <75343703+jojoford@users.noreply.github.com>
2021-05-16 13:01:40 -0500

    add apollo to server

--- server/server.js
+++ server/server.js
@@ -2,10 +2,24 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+//import Apollo
+const { ApolloServer } = require('apollo-server-express');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
+//change to false?
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -17,5 +31,9 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
